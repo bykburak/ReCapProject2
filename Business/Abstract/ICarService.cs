b6ce459d406d0cf00c64a508e2cc0da9adb0c12a
bykburak <burak.buyukkara@gmail.com>
2021-02-02 21:02:21 +0300

    Add files via upload

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+
+       
+
+        List<Car> GetByColorId();
+
+        List<Car> GetByBrandId();
+    }
+}
---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+        ICarDal  _carDal;
+
+
+        public CarManager (ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+
+        List<Car> ICarService.GetByBrandId()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Car> ICarService.GetByColorId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
